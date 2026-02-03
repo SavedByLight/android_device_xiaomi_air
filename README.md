e93a551ee0208493cd10ceb65676057ca1b08a93
Jamie Falconer <mrfluffyoven@gmail.com>
2026-02-03 09:51:41 +0000

    Add Initial Tree

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# android_device_xiaomi_air
-Xiaomi Redmi 13C | Initial
+# Android device tree for Redmi air (air)
+
+## Notes (Contributor)
+ - fstab to be reworked
+ - bootctrl most likely needed
+ - [Dump](https://gitlab.com/SavedByLight/android_dump_xiaomi_air)
+
+```
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ twrp_air.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from air device
+$(call inherit-product, device/xiaomi/air/device.mk)
+
+PRODUCT_DEVICE := air
+PRODUCT_NAME := twrp_air
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := air
+PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
